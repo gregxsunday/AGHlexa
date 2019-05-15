a63052d21c8f289bd3af03901f028a0a52addd22
unknown <gregxsunday@gmail.com>
2019-05-15 21:54:56 +0200

    added weather module

--- weather.py
+++ weather.py
@@ -2,10 +2,27 @@ import webbrowser
 import requests
 import json
 
+class Weather:
+    def __init__(self, city, api_key):
+        resp = requests.get('http://api.openweathermap.org/data/2.5/weather?q={},pl&APPID={}'.format(city, api_key))
+        print(resp.text)
+        json_data = json.loads(resp.text)
+        self.extract(json_data)
+
+    def extract(self, json_data):
+        self.desc = json_data['weather'][0]['main']
+        self.temp = (json_data['main']['temp'] - 273.15) * 10 // 1 / 10
+        self.wind = json_data['wind']['speed']
+
+    def print_weather(self):
+        print('{desc}, temperature is currently {temp} Celcius degrees, wind speed {wind} m/s'.format(
+            desc=self.desc, temp=self.temp,
+            wind=self.wind))
+
+
 def show_the_weather(city=''):
     webbrowser.open('https://www.google.com/search?q=pogoda {}'.format(city))
 
-def get_the_weather(city, api_key):
-    resp = requests.get('http://api.openweathermap.org/data/2.5/weather?q={},pl&APPID={}'.format(city, api_key))
-    json_data = json.loads(resp.text)
-    print('{desc}, temperature is currently {temp} Celcius degrees, wind speed {wind} m/s'.format(desc=json_data['weather'][0]['main'], temp=(json_data['main']['temp'] - 273.15)*10//1/10, wind=json_data['wind']['speed']))
+def print_the_weather(city, api_key):
+    weather = Weather(city, api_key)
+    weather.print_weather()
