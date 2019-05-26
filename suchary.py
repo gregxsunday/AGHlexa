5505d1a33f059fd50132749cb558db063da8d83a
unknown <gregxsunday@gmail.com>
2019-05-26 12:59:06 +0200

    added suchary

---
+++ suchary.py
@@ -0,0 +1,17 @@
+import requests
+import bs4
+import sys
+
+#WAKEWORD suchar
+def suchar():
+    try:
+        resp = requests.get('http://piszsuchary.pl/losuj')
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
+    soup = bs4.BeautifulSoup(resp.text, 'html.parser')
+
+    suchar = soup.find_all('pre')[0].get_text()
+
+    return suchar[:suchar.find('|')]
+
