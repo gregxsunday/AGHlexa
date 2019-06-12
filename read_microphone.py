1e1bc728877b40b874448527ee1b6d759e2a3416
unknown <gregxsunday@gmail.com>
2019-06-12 20:45:48 +0200

    refactor

--- read_microphone.py
+++ read_microphone.py
@@ -1,40 +1,34 @@
-import queue
+from queue import Queue
 import sys
-import tempfile
-import time as timetime
+from tempfile import mktemp
 from statistics import mean
 from collections import deque
-import os
+import sounddevice as sd
+import soundfile as sf
+import numpy
+
 
 def record_audio_no_duration():
     try:
-        import sounddevice as sd
-        import soundfile as sf
-        import numpy  # Make sure NumPy is loaded before it is used in the callback
-        assert numpy  # avoid "imported but unused" message (W0611)
-
-        # input('Press enter to start recording')
-        filename = tempfile.mktemp(prefix='alexa_temp_audio_', suffix='.wav', dir='')
+        filename = mktemp(prefix='alexa_temp_audio_', suffix='.wav', dir='')
 
         device_info = sd.query_devices(None, 'input')
         # soundfile expects an int, sounddevice provides a float:
         samplerate = int(device_info['default_samplerate'])
 
         channels = 1
-        q = queue.Queue()
-        start = timetime.time()
+        q = Queue()
         global flag
         flag = True
-        vol = {}
         last_two_seconds = deque()
         global started
         started = False
 
         def callback(indata, frames, time, status):
+            """This is called (from a separate thread) for each audio block."""
             global started
             global flag
             threshold = 5
-            """This is called (from a separate thread) for each audio block."""
             if status:
                 print(status, file=sys.stderr)
             volume_norm = int(numpy.linalg.norm(indata) * 10)
@@ -43,23 +37,11 @@ def record_audio_no_duration():
             last_two_seconds.append(volume_norm)
             avg = mean(last_two_seconds)
             if len(last_two_seconds) > 39*2:
-                # and avg > threshold:
                 started = True
 
             if started and avg < threshold:
                 flag = False
-            # cr_time = timetime.time()
-            # # delta = int(cr_time - start)
-            # # if delta not in vol:
-            # #     vol[delta] = []
-            # #     if delta != 0:
-            # #         print(delta, mean(vol[delta - 1]), len(vol[delta - 1]))
-            # # vol[delta].append(volume_norm)
-            # global flag
-            # if int(cr_time - start) > 5:
-            #     flag = False
 
-            # print("|" * int(volume_norm))
             q.put(indata.copy())
 
         # Make sure the file is opened before recording anything:
@@ -67,22 +49,12 @@ def record_audio_no_duration():
                           channels=channels, subtype='PCM_16') as file:
             with sd.InputStream(samplerate=samplerate,
                                 channels=channels, callback=callback):
-                # print('#' * 80)
-                # print('press Ctrl+C to stop the recording')
-                # print('#' * 80)
                 while flag:
                     file.write(q.get())
 
     except KeyboardInterrupt:
         print('\nRecording finished: ' + repr(filename))
     return filename
-    # from google_api import transcribe
-    # cmd = transcribe(filename)
-    # os.remove(filename)
-    # return cmd.lower()
-    # print(cmd)
-
-    # return filename
 
 
 if __name__ == '__main__':
